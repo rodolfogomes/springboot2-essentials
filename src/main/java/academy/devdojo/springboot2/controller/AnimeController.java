205dc83dc3b3028caf4360274a4b2278dbf65b11
rodolfogomes <rodolfoaguiargomes@gmail.com>
2021-01-16 09:01:10 -0300

    aula10

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -3,14 +3,13 @@ package academy.devdojo.springboot2.controller;
 import academy.devdojo.springboot2.domain.Anime;
 import academy.devdojo.springboot2.service.AnimeService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Log4j2
+
 @RestController
 @RequestMapping("animes")
 @RequiredArgsConstructor
@@ -33,8 +32,11 @@ public class AnimeController {
         return  new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
 
-
-
+    @DeleteMapping
+    public ResponseEntity<?> delete(@PathVariable long id){
+        animeService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
 }
 
